1b360b9511fb69f414d6cbfcbf5d6a71bbca65f5
Huntless937 <fidailveliyev09.gamil.com>
2026-04-21 11:58:00 +0400

    fix: add junit reporter to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,22 @@
 module.exports = {
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: '.', outputFile: 'junit.xml' }]
+  ],
   projects: [
     {
       displayName: 'server',
       testEnvironment: 'node',
-      testMatch: [
-        '**/tests/server.test.js',
-        '**/tests/integration.test.js'
-      ],
-      collectCoverageFrom: [
-        'server.js',
-        '!**/node_modules/**',
-        '!**/coverage/**'
-      ],
+      testMatch: ['**/tests/server.test.js', '**/tests/integration.test.js'],
+      collectCoverageFrom: ['server.js', '!**/node_modules/**', '!**/coverage/**'],
       coverageDirectory: 'coverage/server',
       setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
     },
     {
       displayName: 'client',
       testEnvironment: 'jsdom',
-      testMatch: [
-        '**/tests/client.test.js'
-      ],
-      collectCoverageFrom: [
-        'public/script.js',
-        '!**/node_modules/**',
-        '!**/coverage/**'
-      ],
+      testMatch: ['**/tests/client.test.js'],
+      collectCoverageFrom: ['public/script.js', '!**/node_modules/**', '!**/coverage/**'],
       coverageDirectory: 'coverage/client',
       setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
     }
